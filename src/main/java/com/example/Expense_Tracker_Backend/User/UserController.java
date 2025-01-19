35b751ddef71670b30de8cd5bb827d539d6addad
gyanavardhana <gyanavardhanmamidisetti@gmail.com>
2025-01-19 12:46:05 +0530

    security miss config corrected(new udpate)

--- src/main/java/com/example/Expense_Tracker_Backend/User/UserController.java
+++ src/main/java/com/example/Expense_Tracker_Backend/User/UserController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 @RequestMapping("/users")
 public class UserController {
@@ -44,6 +45,5 @@ public class UserController {
         userService.removeUser(id);
         return new ResponseEntity<>("User Deleted Successfully", HttpStatus.OK);
     }
-
     
 }
