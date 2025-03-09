34b69271e23fec5cc2dc74af61e71f9e8d368b4b
gyanvardhan.m <gyanvardhan.m@hyperver.co>
2025-03-09 23:16:34 +0530

    fix(env): check env config for render

--- src/main/java/com/example/Expense_Tracker_Backend/ExpenseTrackerBackendApplication.java
+++ src/main/java/com/example/Expense_Tracker_Backend/ExpenseTrackerBackendApplication.java
@@ -2,14 +2,10 @@ package com.example.Expense_Tracker_Backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import io.github.cdimascio.dotenv.Dotenv;
-
 @SpringBootApplication
 public class ExpenseTrackerBackendApplication {
 
 	public static void main(String[] args) {
-		Dotenv dotenv = Dotenv.load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 		SpringApplication.run(ExpenseTrackerBackendApplication.class, args);
 	}
 
